defd4bde06badb5c1000086eca49c1cc456844bc
Benstitou Sofiane <129552238+benstitousofiane@users.noreply.github.com>
2023-08-27 14:56:00 +0200

    Add files via upload

---
+++ recreation/main.js
@@ -0,0 +1,12 @@
+function ouverture(){
+    document.getElementById('formulaireOuverture').submit();
+}
+function ouvertureCours(){
+    document.getElementById('formulaireOuvertureCours').submit();
+}
+function ouvertureDevoirs(){
+    document.getElementById('formulaireOuvertureDevoirs').submit();
+}
+function ouvertureExercices(){
+    document.getElementById('formulaireOuvertureExercices').submit();
+}
